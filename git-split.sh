966ae7f75f41bb087d89a831c8b830ceea0ad4c6
Izzy Gomez <izzygmit@gmail.com>
2023-09-14 01:13:51 -0400

    edited 'git-split.sh' on https://gist.github.com/c4efca57c2237277e3f725b612608b6b on 2023-09-14 01:13:51 -0400
    this commit was auto-generated by `import_gists.py`, which can be found at https://github.com/izzygomez/gists

---
+++ git-split.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Quick script to split a file in `git` into two files while preserving blame
+# history, which is useful for e.g. code blame view on Github.com.
+#
+# Inspired by answers [1] & [2] from Stack Overflow.
+#
+# Usage:
+#   - From clean working directory in your `main` branch, create a new splitting
+#     branch: `git checkout -b split-NEW-FILE-from-ORIGINAL-FILE`
+#   - Run this script: `git-split.sh ORIGINAL-FILE NEW-FILE`
+#   - Verify `git log` shows all "[Tool commit]" commits created through from
+#     script below.
+#   - Edit ORIGINAL-FILE & NEW-FILE to only contain contents they should now
+#     contain*.
+#   - Commit new changes: `git add . && git commit -m "Split ORIGINAL-FILE &
+#     NEW-FILE definitions"`
+#   - Push changes to remote directory: `git push`
+#   - Verify history & blame looks correct on remote repo, like Github.com. 
+#
+#
+# * For example, if ORIGINAL-FILE was a file that contained definitions for two
+#   classes A & B, & the refactor is to split these into separate files, then
+#   ORIGINAL-FILE should have class B's definition deleted & NEW-FILE should
+#   have class A's definition deleted. Put differently, the state of file
+#   contents should be the following:
+#
+#   { ORIGINAL-FILE: {A, B} } ==> { ORIGINAL-FILE: {A}, NEW-FILE: {B} }
+#
+#
+# [1] https://stackoverflow.com/a/53849651
+# [2] https://stackoverflow.com/a/53849613
+
+if [[ $# -ne 2 ]] ; then
+    echo "Usage: git-split.sh original-file new-file"
+    exit 0
+fi
+
+git mv "$1" "$2"
+git commit -n -m "[Tool Commit] Split history $1 to $2 - rename original-file to new-file"
+REV=`git rev-parse HEAD`
+git reset --hard HEAD^
+git mv "$1" temp
+git commit -n -m "[Tool Commit] Split history $1 to $2 - rename original-file to temp"
+git merge $REV
+git commit -a -n -m "[Tool Commit] Split history $1 to $2 - resolve conflict and keep both files"
+git mv temp "$1"
+git commit -n -m "[Tool Commit] Split history $1 to $2 - restore name of original-file"
